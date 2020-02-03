c8223655e0a87b5606b91cd347cd8e29c348fcff
LPMartinez <lpmartinez1109@gmail.com>
2020-02-03 12:40:54 -0600

    Edits made

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,66 +1,43 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../model");
-
-var passengers = [];
-var passByID = [234, 686, 194, 807, 357];
-
-for (i = 0; i < passByID.length; i++){
-    db.Passenger.findAll({
-        where:{
-            id: passByID[i]
-        }
-        
-    }).then(passenger => {
-        passengers.push(passenger[0].dataValues);
-        
-    });
-    
-}
-
-router.get("/passenger", function(req, res) {
-    // console.log(passengers);
-    res.render("passenger", passengers);
-    console.log(passengers);
-});
-
+var passByID = [234, 686, 536, 807, 357];
 router.get("/", function(req, res) {
     res.render('index');
-
 });
-
+router.get("/passenger", function(req, res) {
+        console.log("Hitting Route")
+        db.Passenger.findAll({
+                where:{
+                Passengerid: passByID
+                }
+        }).then(passenger => {
+                console.log(passenger)
+                res.render("passenger", {passengers:passenger});
+        });
+});
 router.get("/timeline", function(req,res){
     res.render("timeline");
-
 });
-
 router.get("/timeline/information", function(req,res){
-    res.render("information")
-    
+        res.render("information")
 });
-
 router.get("/timeline/ship", function(req,res){
-    res.render("ship")
+        res.render("ship")
 });
-
 router.get("/timeline/class", function(req,res){
-    res.render("class")
+        res.render("class")
 });
-
 router.get("/timeline/activities", function(req,res){
-    res.render("activities")
+        res.render("activities")
 });
-
 router.get("/timeline/nightlife", function(req,res){
-    res.render("nightlife")
+        res.render("nightlife")
 });
-
 router.get("/timeline/sink", function(req,res){
-    res.render("sink")
+        res.render("sink")
 });
-
 router.get("/fate", function(req,res){
-    res.render("fate")
+        res.render("fate")
 });
-
-module.exports = router;
+module.exports = router
